6eb511e97e02ef9b097c79ed6fe68caef41ed965
Rocio Yovera <rocioyovera@gmail.com>
2016-12-28 06:55:10 -0500

    finalizado

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,29 @@
-function calcular()
+function evaluar()
 {	
-	var number1 = document.getElementById("number1").value;
-	var diff = document.getElementById("diff");
+	var numberA = document.getElementById("numberA").value;
+	var numberB = document.getElementById("numberB").value;
+	var salida1 = document.getElementById("salida1");
+	var salida2 = document.getElementById("salida2");
 
-	if(number1>21)
+	var suma = parseFloat(numberA)+ parseFloat(numberB);
 
-
+	if (numberA == "" || numberB == ""){
+		salida2.innerHTML = "completar";
+	}
+	else{
+		if (numberA == 10 || numberB == 10) {
+			salida1.innerHTML = suma;
+			salida2.innerHTML = "Muy bien!";
+		}
+		if (suma == 10) {
+			salida1.innerHTML = suma;
+			salida2. innerHTML = "Muy bien!";
+		}
+		else{
+			salida1.innerHTML = suma;
+			salida2.innerHTML = "Inténtalo una vez más";
+		}
+	}
 }
 
 
